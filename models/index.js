a84886c4b6e17a85ddea68bcd24cdbbe3ee40463
Jin-MacBook <taejin7824@gmail.com>
2020-12-18 22:35:32 +0900

    I never use RDS

--- models/index.js
+++ models/index.js
@@ -8,15 +8,7 @@ const User = require( './user' ),
 
 const db = { };
 
-const sequelize = new Sequelize( config.database, config.username, config.password, {
-	host: 'database-1.cqppsabzq26i.ap-northeast-2.rds.amazonaws.com',
-	port: 3306,
-	logging: console.log,
-	maxConcurrentQueries: 100,
-	dialect: 'mysql',
-	pool: { maxConnections: 5, maxIdleTime: 30 },
-	language: 'en'
-} );
+const sequelize = new Sequelize( config.database, config.username, config.password, config );
 
 db.sequelize = sequelize;
 db.User = User;
